68438006b4c79d6fc91f8166c7584ae5d8922bb3
miracyorulmaz <mbyorulmaz@gmail.com>
2026-08-03 23:02:46 +0300

    Add Meta Pixel tracking

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import CoachingStaff from './components/CoachingStaff';
 import FloatingSocials from './components/FloatingSocials';
 import GallerySection from './components/GallerySection';
 import HeroSection from './components/HeroSection';
+import MetaPixel from './components/MetaPixel';
 import RegistrationForm from './components/RegistrationForm';
 import SiteFooter from './components/SiteFooter';
 import SiteHeader from './components/SiteHeader';
@@ -16,6 +17,7 @@ export default async function Home() {
   const { settings, programs, coaches, gallery } = await getPublicContent();
   return (
     <main className="relative overflow-hidden bg-black">
+      <MetaPixel />
       <SiteHeader />
       <div className="pointer-events-none fixed inset-0 z-0 hidden overflow-hidden md:block"><div className="absolute -left-8 inset-y-0 w-[6px] bg-white/10" /><div className="absolute left-0 inset-y-0 w-[2px] bg-white/5" /><div className="absolute -right-8 inset-y-0 w-[6px] bg-white/10" /><div className="absolute right-0 inset-y-0 w-[2px] bg-white/5" /></div>
       <div className="relative z-10"><HeroSection title={settings.hero_title} description={settings.hero_description} /><TrustAndBranding /><GallerySection images={gallery} /><SocialMediaSection settings={settings} images={gallery} /><TrainingPrograms programs={programs} /><AcademyFeatures /><CoachingStaff coaches={coaches} /><RegistrationForm programs={programs} registrationOpen={settings.registration_open} /><VisitSection settings={settings} /><SiteFooter settings={settings} /></div>
